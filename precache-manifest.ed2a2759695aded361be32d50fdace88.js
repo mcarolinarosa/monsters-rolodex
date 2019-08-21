86e62a2e843b2b4e40534130f0ce9386957105ad
Carolina Rosa <carolinarosa@novatronica.com>
2019-08-21 11:33:12 +0100

    Updates

---
+++ precache-manifest.ed2a2759695aded361be32d50fdace88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef9730da99574c548f555d35277155b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d04e8318a8f019ee301e",
+    "url": "/monsters-rolodex/static/css/main.4674fa26.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "d04e8318a8f019ee301e",
+    "url": "/monsters-rolodex/static/js/main.297c442e.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
